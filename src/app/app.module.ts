326d5148f28c2460de111db92338ff915be7968c
Ki Jung Kim <kijungkim@eworldes.com>
2019-09-10 09:49:23 -1000

    injected home and play components into app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { PlayComponent } from './pages/play/play.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PlayComponent
   ],
   imports: [
     BrowserModule,
