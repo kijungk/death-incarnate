a40e9f44119606e0025c8430b3ee51a916db4b06
Ki Jung Kim <kijungkim@eworldes.com>
2019-09-10 09:47:46 -1000

    added in home and play routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PlayComponent } from './pages/play/play.component';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'play', component: PlayComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
